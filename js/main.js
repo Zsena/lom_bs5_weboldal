f46c5d56b91924655f36eb38123544cf74ad7e8e
Zsanett Tamás <zsanett.tamas87@gmail.com>
2022-08-18 03:30:23 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -81,17 +81,6 @@
             }
         }
     });
-
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
-        $(this).addClass('active');
-
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
-    });
     
 })(jQuery);
 
